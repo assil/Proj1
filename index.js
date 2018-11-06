9befd1dee0f644af08534398950bcc4b387841b7
assil <assilalsheik@hotmail.de>
2018-11-06 19:38:49 +0100

    find was added

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ app.get('/', (req, res) => {
   request('https://data-asg.goldprice.org/dbXRates/USD,EUR', function (error, response, body) {
     const json = JSON.parse(body)
     const price = _.get(json, 'items[0].curr')
-    const itemsvalue = _.get(json, 'items', ['0', '1']);
+    const itemsvalue = _.find('items[0]', function(o) {
+        return o.curr
+    })
 
     console.log(itemsvalue)
-    res.send(200, price)
+    res.send(200).send(price)
+    //res.send(200, price) was undefined by npm start and changed
     })
 })
 
