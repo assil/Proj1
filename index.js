35a44212a95d2bb062bdaaf052e7e3c71d33bb76
assil <assil@hotmail.de>
2018-12-19 20:47:48 +0100

    back2square1

--- index.js
+++ index.js
@@ -7,15 +7,11 @@ const app = express()
 app.get('/', (req, res) => {
   request('https://data-asg.goldprice.org/dbXRates/USD,EUR', function (error, response, body) {
     const json = JSON.parse(body)
-    //const price = _.get(json, 'items[0].curr')
-    //const itemsvalue = .find(({ curr }) => curr === 'EUR')
-    const { items } = await response.json()
-    const euro = items.find(({ curr }) => curr === 'EUR')
-    const dollar = items.find(({ curr }) => curr === 'USD')
-    
+    const price = _.get(json, 'items[0].curr')
+    //const euro = items.find(({ curr }) => curr === 'EUR')
+
     res.send(200, price)
-    //res.send(200, price) was undefined by npm start and was changed
-    console.log(euro, dollar)
+    console.log(euro)
     })
 })
 
